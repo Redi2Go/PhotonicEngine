e4d9149db3b10474ed23932158a289b555a21143
Essentuan <essentuan@gmail.com>
2026-04-01 18:18:38 -0500

    Add initial 1.211.11 fabric entrypoint

---
+++ modules/versions/1_21_11/fabric/src/main/java/at/redi2go/photonics/client/PhotonicsClientFabric.java
@@ -0,0 +1,22 @@
+package at.redi2go.photonics.client;
+
+import at.redi2go.photonics.core.Photonics;
+import com.vdurmont.semver4j.Semver;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+
+import java.util.Optional;
+
+public class PhotonicsClientFabric implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        Optional<ModContainer> photonics = FabricLoader.getInstance().getModContainer("photonics");
+        if (photonics.isEmpty()) throw new IllegalStateException("Where is photonics? :(");
+
+        Photonics.init(
+                new Semver(photonics.get().getMetadata().getVersion().getFriendlyString()),
+                FabricLoader.getInstance().isDevelopmentEnvironment()
+        );
+    }
+}
