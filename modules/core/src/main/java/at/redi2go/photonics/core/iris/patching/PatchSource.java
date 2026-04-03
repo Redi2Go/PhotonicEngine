2efffac8b09248a5eb1ba2d3b1811e94e7d6a689
Essentuan <essentuan@gmail.com>
2026-04-03 10:08:01 -0500

    Add 'onChanged' method to PatchSource
    Allows to only reload patches when required

--- modules/core/src/main/java/at/redi2go/photonics/core/iris/patching/PatchSource.java
+++ modules/core/src/main/java/at/redi2go/photonics/core/iris/patching/PatchSource.java
@@ -1,9 +1,13 @@
 package at.redi2go.photonics.core.iris.patching;
 
+import at.redi2go.photonics.api.Disposable;
+
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Stream;
 
 public interface PatchSource {
     Stream<Path> streamPatches();
+
+    void onChanged(Runnable listener);
 }
