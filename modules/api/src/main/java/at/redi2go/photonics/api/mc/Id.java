047b9d08ab918ea75e4b88569f56c8ddf1877a26
Essentuan <essentuan@gmail.com>
2026-04-16 10:02:49 -0500

    Add namespace/path to Id

--- modules/api/src/main/java/at/redi2go/photonics/api/mc/Id.java
+++ modules/api/src/main/java/at/redi2go/photonics/api/mc/Id.java
@@ -1,6 +1,10 @@
 package at.redi2go.photonics.api.mc;
 
 public interface Id {
+    String namespace();
+
+    String path();
+
     static Id fromNamespaceAndPath(String namespace, String path) {
         throw new AssertionError(); // TO BE IMPLEMENTED BY MIXIN
     }
--- modules/versions/1_21_11/common/src/impl/mixins/at/redi2go/photonics/impl/mixins/mc/IdentifierMixin.java
+++ modules/versions/1_21_11/common/src/impl/mixins/at/redi2go/photonics/impl/mixins/mc/IdentifierMixin.java
@@ -2,8 +2,27 @@ package at.redi2go.photonics.impl.mixins.mc;
 
 import at.redi2go.photonics.api.mc.Id;
 import net.minecraft.resources.Identifier;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(Identifier.class)
 public abstract class IdentifierMixin implements Id {
+    @Shadow
+    @Final
+    private String namespace;
+
+    @Shadow
+    @Final
+    private String path;
+
+    @Override
+    public String namespace() {
+        return namespace;
+    }
+
+    @Override
+    public String path() {
+        return path;
+    }
 }
