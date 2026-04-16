4f2e12ea8f1bd2bde913eb5cd4872b05d1382a99
Essentuan <essentuan@gmail.com>
2026-04-16 10:02:59 -0500

    Add BlockMesher registry

--- modules/core/src/main/java/at/redi2go/photonics/core/rendering/world/bakery/BlockMesher.java
+++ modules/core/src/main/java/at/redi2go/photonics/core/rendering/world/bakery/BlockMesher.java
@@ -1,10 +1,19 @@
 package at.redi2go.photonics.core.rendering.world.bakery;
 
+import at.redi2go.photonics.api.mc.Id;
 import at.redi2go.photonics.api.mc.core.IBlockPos;
+import at.redi2go.photonics.api.mc.world.level.IBlock;
 import at.redi2go.photonics.api.mc.world.level.IBlockAndTintGetter;
 import at.redi2go.photonics.api.mc.world.level.IBlockState;
+import it.unimi.dsi.fastutil.objects.Object2ObjectAVLTreeMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+
+import java.util.Optional;
 
 public interface BlockMesher {
+    Registry REGISTRY = new Registry();
+
     /**
      * Meshes a block at {@code pos} with {@code blockState}.
      *
@@ -16,4 +25,26 @@ public interface BlockMesher {
             IBlockAndTintGetter blockAndTintGetter,
             VertexBuilder vertexBuilder
     );
+
+    class Registry {
+        private final Object2ObjectMap<Id, BlockMesher> blockRegistry = new Object2ObjectOpenHashMap<>();
+        private final Object2ObjectMap<String, BlockMesher> namespaceRegistry = new Object2ObjectOpenHashMap<>();
+
+        Registry() {
+
+        }
+
+        public void addBlock(Id id, BlockMesher mesher) {
+            blockRegistry.put(id, mesher);
+        }
+
+        public void addNamespace(String namespace, BlockMesher mesher) {
+            namespaceRegistry.put(namespace, mesher);
+        }
+
+        public Optional<BlockMesher> get(IBlock block) {
+            return Optional.ofNullable(blockRegistry.get(block.id()))
+                    .or(() -> Optional.of(namespaceRegistry.get(block.id().namespace())));
+        }
+    }
 }
