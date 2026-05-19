2be22fd8d45eb5cefebb87c572b0983c6adffd0a
Essentuan <essentuan@gmail.com>
2026-05-19 03:47:42 -0500

    Fix missing method impl
    ??????

--- modules/versions/1_21_11/common/src/impl/mixins/at/redi2go/photonics/impl/mixins/mc/commands/arguments/blocks/BlockStateParser$BlockResultMixin.java
+++ modules/versions/1_21_11/common/src/impl/mixins/at/redi2go/photonics/impl/mixins/mc/commands/arguments/blocks/BlockStateParser$BlockResultMixin.java
@@ -1,14 +1,18 @@
 package at.redi2go.photonics.impl.mixins.mc.commands.arguments.blocks;
 
+import at.redi2go.photonics.api.mc.IProperty;
 import at.redi2go.photonics.api.mc.commands.arguments.blocks.IBlockStateParser;
 import at.redi2go.photonics.api.mc.nbt.ICompoundTag;
 import at.redi2go.photonics.api.mc.world.level.IBlockState;
 import net.minecraft.commands.arguments.blocks.BlockStateParser;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
 import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Map;
 
 @Mixin(BlockStateParser.BlockResult.class)
 public abstract class BlockStateParser$BlockResultMixin implements IBlockStateParser.BlockResult {
@@ -17,6 +21,13 @@ public abstract class BlockStateParser$BlockResultMixin implements IBlockStatePa
     @Override
     public IBlockState blockState() {
         return (IBlockState) shadow$blockState();
+    }
+
+    @Shadow public abstract Map<Property<?>, Comparable<?>> shadow$properties();
+
+    @Override
+    public Map<IProperty<?>, Comparable<?>> properties() {
+        return (Map<IProperty<?>, Comparable<?>>) (Map<?, ?>) shadow$properties();
     }
 
     @Shadow public abstract CompoundTag shadow$nbt();
