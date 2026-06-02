86c8fedf20e8a6ccbc821cf4efad02c4fb1a320b
Essentuan <essentuan@gmail.com>
2026-06-02 16:44:28 -0500

    Fix shouldIgnore check

--- modules/core/src/main/java/at/redi2go/photonics/core/rendering/world/IgnoredInterruptedException.java
+++ modules/core/src/main/java/at/redi2go/photonics/core/rendering/world/IgnoredInterruptedException.java
@@ -2,6 +2,8 @@ package at.redi2go.photonics.core.rendering.world;
 
 public class IgnoredInterruptedException extends RuntimeException {
     public static boolean shouldIgnore(Throwable t) {
+        if (t == null) return true;
+
         while (t != null) {
             if (t instanceof IgnoredInterruptedException) return true;
             t = t.getCause();
