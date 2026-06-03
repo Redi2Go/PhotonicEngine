577ccebaec35165ca5cdbd2136a34902de93fc02
Essentuan <51426319+Essentuan@users.noreply.github.com>
2026-06-03 11:39:20 -0500

    Fix projection

--- modules/shaders/photonics/utility/projection.glsl
+++ modules/shaders/photonics/utility/projection.glsl
@@ -1,12 +1,16 @@
 #ifndef PH_PROJECTION_UTILITY_INCLUDE
 #define PH_PROJECTION_UTILITY_INCLUDE
 
+vec4 ph_project(mat4 m, vec3 pos) {
+    return vec4(m[0].x, m[1].y, m[2].zw) * pos.xyzz + m[3];
+}
+
 vec3 ph_transform(mat4 m, vec3 pos) {
     return mat3(m) * pos + m[3].xyz;
 }
 
 vec3 ph_project_and_divide(mat4 m, vec3 pos) {
-    vec4 homogenous = project(m, pos);
+    vec4 homogenous = ph_project(m, pos);
     return homogenous.xyz / homogenous.w;
 }
 
@@ -14,7 +18,7 @@ vec3 ph_reproject_player_pos(vec3 player_pos, bool hand, vec2 jitter, out vec3 p
     vec3 camera_offset = hand ? vec3(0.0f) : cameraPosition - previousCameraPosition;
     previous_player_pos = player_pos + camera_offset;
 
-    vec3 previous_pos = transform(gbufferPreviousModelView, previous_player_pos);
+    vec3 previous_pos = ph_transform(gbufferPreviousModelView, previous_player_pos);
     previous_pos = ph_project_and_divide(gbufferPreviousProjection, previous_pos);
     previous_pos.xy+= jitter;
 
