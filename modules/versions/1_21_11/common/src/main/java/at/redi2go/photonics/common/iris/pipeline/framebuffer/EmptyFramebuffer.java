9ee0fdb5889af84ebfe0782df59eb9d29b6fa594
Essentuan <51426319+Essentuan@users.noreply.github.com>
2026-06-14 20:56:07 -0500

    Fix EmtpyFramebuffer not inheriting InternalIrisFramebuffer

--- modules/versions/1_21_11/common/src/main/java/at/redi2go/photonics/common/iris/pipeline/framebuffer/EmptyFramebuffer.java
+++ modules/versions/1_21_11/common/src/main/java/at/redi2go/photonics/common/iris/pipeline/framebuffer/EmptyFramebuffer.java
@@ -1,12 +1,21 @@
 package at.redi2go.photonics.common.iris.pipeline.framebuffer;
 
 import at.redi2go.photonics.core.iris.pipeline.texture.IrisFramebuffer;
+import org.joml.Vector2i;
+import org.joml.Vector2ic;
 
-public class EmptyFramebuffer implements IrisFramebuffer {
+public class EmptyFramebuffer implements IrisFramebuffer, InternalIrisFramebuffer {
+    private final static Vector2ic VIEWPORT_SIZE = new Vector2i(1, 1);
+
     public static final EmptyFramebuffer INSTANCE = new EmptyFramebuffer();
 
     private EmptyFramebuffer() {
 
+    }
+
+    @Override
+    public Vector2ic viewportSize() {
+        return VIEWPORT_SIZE;
     }
 
     @Override
@@ -16,6 +25,16 @@ public class EmptyFramebuffer implements IrisFramebuffer {
 
     @Override
     public void recalculateSizes() {
+
+    }
+
+    @Override
+    public void bind() {
+
+    }
+
+    @Override
+    public void unbind() {
 
     }
 }
