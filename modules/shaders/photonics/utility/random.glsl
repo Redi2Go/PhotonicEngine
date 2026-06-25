73c9187daf3cfe93343946d71aef38dc063976c6
Essentuan <51426319+Essentuan@users.noreply.github.com>
2026-06-24 22:29:42 -0500

    Reduce shadow softness

--- modules/shaders/photonics/utility/random.glsl
+++ modules/shaders/photonics/utility/random.glsl
@@ -29,7 +29,7 @@ int ph_rand_next_int(inout uint rand_state, float min, float max) {
     return int(min + (ph_rand_next_float(rand_state) * (max - min)));
 }
 
-const float ph_light_jitter_radius = 1.0f / 8.0f;
+const float ph_light_jitter_radius = 1.0f / 16.0f;
 
 void ph_rand_sample_position(inout uint rand_state, inout vec3 light_position, vec3 sample_pos) {
     light_position = floor(light_position) + 0.5f;
