016b4ef3ac5f6e4fcf4c6b5fb5978a17f2d6a27c
Essentuan <51426319+Essentuan@users.noreply.github.com>
2026-07-11 10:29:16 -0500

    Reserve order of denoiser passes
    Improves a lot, who would've though lurking in trainee-chat helped with something

--- modules/core/src/main/java/at/redi2go/photonics/core/iris/extensions/RestirPipeline.java
+++ modules/core/src/main/java/at/redi2go/photonics/core/iris/extensions/RestirPipeline.java
@@ -65,10 +65,10 @@ public class RestirPipeline extends AbstractPhotonicsExtension {
                 .when(this::isDenoisingEnabled, b0 -> {
                     b0.withFramebuffer(denoiseFramebuffer);
                     b0.debugGroup("svgf");
-                    b0.thenRun(() -> atrousIteration = 0);
+                    b0.thenRun(() -> atrousIteration = denoiserPasses);
                     b0.deferredPass("variance prefilter", "/photonics/rendering/restir/passes/r8_variance_prefilter.fsh", null);
                     b0.repeat(denoiserPasses, b1 -> {
-                        b1.thenRun(() -> atrousIteration++);
+                        b1.thenRun(() -> atrousIteration--);
                         b1.thenRun(atrousUpdater::updateNow);
                         b1.thenFlip(denoiseFramebuffer);
                         b1.deferredPass("atrous iteration", "/photonics/rendering/restir/passes/r9_denoising.fsh", null);
