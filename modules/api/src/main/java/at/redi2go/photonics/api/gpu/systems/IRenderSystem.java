f626b7a4c8cd32b7a7e41b4dd40f35d601cca99b
Essentuan <51426319+Essentuan@users.noreply.github.com>
2026-08-16 15:00:50 -0500

    Remove old blaze3d api

--- modules/api/src/main/java/at/redi2go/photonics/api/gpu/systems/IRenderSystem.java
+++
@@ -1,7 +0,0 @@
-package at.redi2go.photonics.api.gpu.systems;
-
-public interface IRenderSystem {
-    static IGpuDevice getDevice() {
-        throw new AssertionError(); // TO BE IMPLEMENTED BY MIXIN
-    }
-}
--- modules/api/src/main/java/at/redi2go/photonics/api/gpu/textures/IGpuTexture3D.java
+++
@@ -1,6 +0,0 @@
-package at.redi2go.photonics.api.gpu.textures;
-
-import org.joml.Vector3ic;
-
-public non-sealed interface IGpuTexture3D extends IGpuTexture<Vector3ic> {
-}
